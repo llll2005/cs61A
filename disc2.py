138a87b4632a3207f4ab8503790208ab16a4f73d
llll2005 <f940510@gmail.com>
2025-09-08 00:18:49 +0800

    re-construct

---
+++ disc2.py
@@ -0,0 +1,14 @@
+'''
+Q1.
+What is the value of result after executing result = (lambda x: 2 * (lambda x: 3)(4) * x)(5)?
+30
+check:
+'''
+print((lambda x: 2 * (lambda x: 3)(4) * x)(5))
+
+'''
+Q2.
+d
+d
+c
+'''
